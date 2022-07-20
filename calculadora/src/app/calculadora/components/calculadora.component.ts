b3d4e85de655c355a698da6601502fc89512e181
KennedyMartinsB <Kennedy.Martins@bandtec>
2022-07-20 13:13:39 -0300

    Feature: consumindo service

--- calculadora/src/app/calculadora/components/calculadora.component.ts
+++ calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -28,4 +28,69 @@ export class CalculadoraComponent implements OnInit {
     this.operacao = null;
   }
 
+  adicionarNumero(numero: string):void{
+    if(this.operacao === null){
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    }else{
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+  concatenarNumero(numAtua: string, numConcat: string): string{
+    if(numAtua === '0' || numAtua === null){
+      numAtua = '';
+    }
+
+    if(numConcat === '.' && numAtua === ''){
+      return '0.';
+    }
+
+    if(numConcat === '.' && numAtua.indexOf('.') > -1){
+      return numAtua;
+    }
+
+    return numAtua + numConcat;
+  }
+
+  definirCalculo(operacao:string): void{
+    if(this.operacao === null){
+      this.operacao = operacao;
+      return;
+    }
+
+    if(this.numero2 !== null){
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao
+      );
+  		this.operacao = operacao;
+  		this.numero1 = this.resultado.toString();
+  		this.numero2 = null;
+  		this.resultado = null;
+    }
+  }
+
+  calcular(): void{
+    if(this.numero2 === null){
+      return;
+    }
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao);
+  }
+
+  get display(): string {
+  	if (this.resultado !== null) {
+  		return this.resultado.toString();
+  	}
+  	if (this.numero2 !== null) {
+  		return this.numero2;
+  	}
+  	return this.numero1;
+  }
+
+
+
 }
